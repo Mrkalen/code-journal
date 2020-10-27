29fe8a9126c6c058ea26cb25b4a99c297b69538f
Mrkalen <mrkalen@hotmail.com>
2020-10-27 10:22:01 -0700

    completed issue-1-create-a-profile Kalen Cobb

--- code-journal/js/main.js
+++ code-journal/js/main.js
@@ -1,19 +1,32 @@
 var $userInput = document.querySelector('#avatar')
-console.log('text content', $userInput);
 
 var $avatar = document.querySelector('img');
-console.log('img queried', $avatar);
-
-var url = $userInput.textContent;
-console.log('text input', url);
 
 $userInput.addEventListener('input', function (event) {
  var url =  event.target.value;
- console.log(url);
   $avatar.setAttribute('src', url);
 });
 
+var $profile = document.querySelector('#profile-form');
 
-$userInput.addEventListener('input', function handleInput(event) {
-  console.log('value of ' + event.target.name + ': ' + event.target.value);
+$profile.addEventListener('submit', function (event) {
+  event.preventDefault();
+  data.profile.avatarUrl = $profile.elements.avatarUrl.value;
+  data.profile.username = $profile.elements.username.value;
+  data.profile.fullName = $profile.elements.fullName.value;
+  data.profile.location = $profile.elements.location.value;
+  data.profile.bio = $profile.elements.bio.value;
+
+  $profile.reset();
+  $avatar.setAttribute('src', "images/placeholder-image-square.jpg");
 });
+
+var savedProfile = localStorage.getItem('javascript-local-storage')
+if (savedProfile !== null) {
+  data = JSON.parse(savedProfile);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var profileData = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', profileData);
+})
